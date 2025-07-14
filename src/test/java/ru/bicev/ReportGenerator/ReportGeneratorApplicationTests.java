652df25bb9a18b2d99000bf538f1e6eefa309144
BicEv <bickbovev@gmail.com>
2025-07-14 18:20:36 +0300

    Basic project structure

---
+++ src/test/java/ru/bicev/ReportGenerator/ReportGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bicev.ReportGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
